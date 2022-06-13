669c2b6b68c71886953d332d52132fa0059c8d1c
haafizh <haafizhhamda@student.telkomuniversity.ac.id>
2022-06-13 13:56:45 -0700

    Menampilkan data dengan adapter

--- app/src/main/java/org/d3if4076/galeri_hewan/MainActivity.kt
+++ app/src/main/java/org/d3if4076/galeri_hewan/MainActivity.kt
@@ -3,6 +3,8 @@ package org.d3if4076.galeri_hewan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import org.d3if4076.galeri_hewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d("MainActivity", "Jumlah data: " + getData().size)
+        with(binding.recyclerView) {
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter = MainAdapter(getData())
+            setHasFixedSize(true)
+        }
     }
     private fun getData(): List<Hewan> {
         return listOf(
