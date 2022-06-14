bd5436bc1eb1af0b79440499b6f3107381d1c870
haafizh <haafizhhamda@student.telkomuniversity.ac.id>
2022-06-14 08:31:33 -0700

    Menambahkan Grid

--- app/src/main/java/org/d3if4076/galeri_hewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if4076/galeri_hewan/ui/main/MainFragment.kt
@@ -1,12 +1,13 @@
 package org.d3if4076.galeri_hewan.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.d3if4076.galeri_hewan.R
 import org.d3if4076.galeri_hewan.data.Hewan
@@ -20,6 +21,7 @@ class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
     private lateinit var myAdapter: MainAdapter
+    private var isLinearLayoutManager = true
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -33,6 +35,7 @@ class MainFragment : Fragment() {
             adapter = myAdapter
             setHasFixedSize(true)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -40,5 +43,41 @@ class MainFragment : Fragment() {
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+    }
+    private fun chooseLayout() {
+        if (isLinearLayoutManager) {
+            binding.recyclerView.layoutManager =
+                LinearLayoutManager(this.requireContext())
+        } else {
+            binding.recyclerView.layoutManager =
+                GridLayoutManager(this.requireContext(), 2)
+        }
+    }
+    private fun setIcon(menuItem: MenuItem?) {
+        if (menuItem == null) return
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(requireContext(),
+                    R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(requireContext(),
+                R.drawable.ic_linear_layout)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.layout_menu, menu)
+        val layoutButton = menu?.findItem(R.id.action_switch_layout)
+        setIcon(layoutButton)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_layout -> {
+                // Sets isLinearLayoutManager to the opposite value
+                isLinearLayoutManager = !isLinearLayoutManager
+                // Sets layout and icon
+                chooseLayout()
+                setIcon(item)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
