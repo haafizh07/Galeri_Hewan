26509cf9c2640c95f7bd1961fca19798d216845c
haafizh <haafizhhamda@student.telkomuniversity.ac.id>
2022-06-14 10:50:50 -0700

    Menambahkan loading indicator

--- app/src/main/java/org/d3if4076/galeri_hewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if4076/galeri_hewan/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import org.d3if4076.galeri_hewan.R
 import org.d3if4076.galeri_hewan.data.Hewan
 import org.d3if4076.galeri_hewan.databinding.FragmentMainBinding
+import org.d3if4076.galeri_hewan.network.ApiStatus
 
 
 class MainFragment : Fragment() {
@@ -22,24 +23,47 @@ class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     private lateinit var myAdapter: MainAdapter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = FragmentMainBinding.inflate(layoutInflater, container, false)
         myAdapter = MainAdapter()
         with(binding.recyclerView) {
             addItemDecoration(
-                DividerItemDecoration(context,
-                RecyclerView.VERTICAL)
+                DividerItemDecoration(
+                    context,
+                    RecyclerView.VERTICAL
+                )
             )
             adapter = myAdapter
             setHasFixedSize(true)
         }
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getData().observe(viewLifecycleOwner, {
             myAdapter.updateData(it)
         })
+        viewModel.getStatus().observe(viewLifecycleOwner, {
+            updateProgress(it)
+        })
+    }
+
+    private fun updateProgress(status: ApiStatus) {
+        when (status) {
+            ApiStatus.LOADING -> {
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            ApiStatus.SUCCESS -> {
+                binding.progressBar.visibility = View.GONE
+            }
+            ApiStatus.FAILED -> {
+                binding.progressBar.visibility = View.GONE
+                binding.networkError.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/org/d3if4076/galeri_hewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4076/galeri_hewan/ui/main/MainViewModel.kt
@@ -9,21 +9,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.d3if4076.galeri_hewan.R
 import org.d3if4076.galeri_hewan.data.Hewan
+import org.d3if4076.galeri_hewan.network.ApiStatus
 import org.d3if4076.galeri_hewan.network.HewanApi
 
 class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
+    private val status = MutableLiveData<ApiStatus>()
     init {
         retrieveData()
     }
     private fun retrieveData() {
         viewModelScope.launch (Dispatchers.IO) {
+            status.postValue(ApiStatus.LOADING)
             try {
                 data.postValue(HewanApi.service.getHewan())
+                status.postValue(ApiStatus.SUCCESS)
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
+                status.postValue(ApiStatus.FAILED)
             }
         }
     }
     fun getData(): LiveData<List<Hewan>> = data
+    fun getStatus(): LiveData<ApiStatus> = status
 }
